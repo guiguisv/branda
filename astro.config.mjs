05f2d27887e734bce07b918e453c98f8a649a6d0
guiguisv <108839984+guiguisv@users.noreply.github.com>
2025-03-18 20:08:39 -0300

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://tubranda.com.ar',
-  integrations: [tailwind()]
-});
-
